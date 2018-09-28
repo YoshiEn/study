ed543401f6b051a15529ffbaabeb1535effe3b19
str32 <str32.h1d1s0n7sc@gmail.com>
2018-09-28 17:39:36 +0900

    𠮷さん

---
+++ study-develop/bot/slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-421996460261-444342181525-1V8QLQrWlD5ZlvxS9UHVKss3"
+
+# どの応答にも当てはまらない場合の応答
+DEFAULT_REPLY = "髙野翔生"
+
+# プラグインスクリプト
+PLUGINS = ['plugins']
